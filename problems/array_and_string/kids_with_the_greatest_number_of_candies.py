f45084deaa440501d84f268d56081fd2afa50a16
Yuliyan Stoilov <yuliyanstoilov@gmail.com>
2024-07-05 17:51:54 +0300

    Create kids_with_the_greatest_number_of_candies.py

---
+++ problems/array_and_string/kids_with_the_greatest_number_of_candies.py
@@ -0,0 +1,14 @@
+def kids_with_candies(candies: list[int], extra_candies: int) -> list[bool]:
+    r = [False] * len(candies)
+    max_candies = max(candies)
+
+    for i in range(len(candies)):
+        if candies[i] + extra_candies >= max_candies:
+            r[i] = True
+
+    return r
+
+
+print(kids_with_candies(candies=[2, 3, 5, 1, 3], extra_candies=3))
+print(kids_with_candies(candies=[4, 2, 1, 1, 2], extra_candies=1))
+print(kids_with_candies(candies=[12, 1, 12], extra_candies=10))
