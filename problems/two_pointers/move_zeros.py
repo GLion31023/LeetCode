871d7cf6a4928e48358a3fb8e6f1f66b00fdc266
Yuliyan Stoilov <yuliyanstoilov@gmail.com>
2024-07-09 18:20:34 +0300

    Update move_zeros.py

--- problems/two_pointers/move_zeros.py
+++ problems/two_pointers/move_zeros.py
@@ -2,9 +2,19 @@ def move_zeroes(nums: list[int]) -> None:
     """
     Do not return anything, modify nums in-place instead.
     """
+    write = 0
+
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[write] = nums[i]
+            write += 1
+
+    while write < len(nums):
+        nums[write] = 0
+        write += 1
 
 
-arr = [0,1,0,3,12]
+arr = [0, 1, 0, 3, 12]
 arr1 = [0]
 move_zeroes(arr)
 move_zeroes(arr1)
