51a5138057744b22a08be4a2d90479143653cee2
Yuliyan Stoilov <yuliyanstoilov@gmail.com>
2024-07-15 16:51:38 +0300

    Create find_pivot_index.py

---
+++ problems/prefix_sum/find_pivot_index.py
@@ -0,0 +1,16 @@
+def pivot_index(nums: list[int]) -> int:
+    total = sum(nums)
+    left = 0
+
+    for n in range(len(nums)):
+        if left == total - nums[n] - left:
+            return n
+        left += nums[n]
+
+    return -1
+
+
+print(pivot_index([1, 7, 3, 6, 5, 6]))
+print(pivot_index([1, 2, 3]))
+print(pivot_index([2, 1, -1]))
+print(pivot_index([1, 2, 3, 4, 6, 0, -6]))  # Let's analyze this case
