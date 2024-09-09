76f13b0a4ec30c916d38d20a4b20fd6ae0a295ec
Yuliyan Stoilov <yuliyanstoilov@gmail.com>
2024-09-09 19:34:43 +0300

    Update palindrome_linked_list.py

--- practice/linked_list/palindrome_linked_list.py
+++ practice/linked_list/palindrome_linked_list.py
@@ -59,3 +59,19 @@ l2 = list_to_ll([1, 2])
 
 print(is_palindrome(l1))
 print(is_palindrome(l2))
+
+# Example 1:
+#
+# Input: head = [1,2,2,1]
+# Output: true
+
+# Example 2:
+#
+# Input: head = [1,2]
+# Output: false
+#
+# Constraints:
+# The number of nodes in the list is in the range [1, 10^5].
+# 0 <= Node.val <= 9
+#
+# Follow up: Could you do it in O(n) time and O(1) space?
