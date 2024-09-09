ab78093dfbeabd5f49d2d0af5c83755432b8e7f4
Yuliyan Stoilov <yuliyanstoilov@gmail.com>
2024-09-09 19:24:38 +0300

    Create palindrome_linked_list.py

---
+++ practice/linked_list/palindrome_linked_list.py
@@ -0,0 +1,61 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __repr__(self):
+        return f"node_value({self.val})"
+
+
+def list_to_ll(lst: list[int]) -> Optional[ListNode]:
+    if not lst:
+        return None
+
+    head = ListNode(lst[0])
+    curr = head
+
+    for v in lst[1:]:
+        curr.next = ListNode(v)
+        curr = curr.next
+
+    return head
+
+
+def is_palindrome(head: Optional[ListNode]) -> bool:
+    if not head.next:
+        return True
+
+    slow = fast = head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    mid = slow
+    prev = None
+
+    while mid:
+        next_n = mid.next
+        mid.next = prev
+        prev = mid
+        mid = next_n
+
+    mid = prev
+
+    while mid:
+        if not mid.val == head.val:
+            return False
+        mid = mid.next
+        head = head.next
+
+    return True
+
+
+l1 = list_to_ll([1, 2, 2, 1])
+l2 = list_to_ll([1, 2])
+
+print(is_palindrome(l1))
+print(is_palindrome(l2))
