25923e76928d8bce559c1412ffd1b762c7fac3a3
Yuliyan Stoilov <yuliyanstoilov@gmail.com>
2024-09-12 16:41:27 +0300

    Update integer_to_roman.py

--- problems/array_and_string/integer_to_roman.py
+++ problems/array_and_string/integer_to_roman.py
@@ -1,6 +1,7 @@
 def int_to_roman(num: int) -> str:
     romans = {1: "I", 4: "IV", 5: "V", 9: "IX",
               10: "X", 40: "XL", 50: "L", 90: "XC",
+    romans = {1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC",
               100: "C", 400: "CD", 500: "D", 900: "CM", 1000: "M"}
 
     roman_n = ''
