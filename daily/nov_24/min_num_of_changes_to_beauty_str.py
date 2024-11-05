81f6e7ade87698e422b9c5a16fa05e3d4b966c84
Yuliyan Stoilov <yuliyanstoilov@gmail.com>
2024-11-05 06:58:22 +0200

    daily Nov 5 min changes to make str beautiful

--- daily/nov_24/min_num_of_changes_to_beauty_str.py
+++ daily/nov_24/min_num_of_changes_to_beauty_str.py
@@ -2,7 +2,7 @@ def min_changes(s: str) -> int:
     changes = 0
 
     for i in range(1, len(s), 2):
-        if s[i] != s[i -1]:
+        if s[i] != s[i - 1]:
             changes += 1
 
     return changes
